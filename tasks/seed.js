c1b50b5242e00b9096cd5e1e8f35cb057c52cb32
nmiah <nmiah>
2019-05-13 16:47:51 -0400

    started seed file

---
+++ tasks/seed.js
@@ -0,0 +1,45 @@
+const dbConnection = require("../data/connection");
+const data = require("../data/");
+const drinks = data.drinks;
+const users = data.users;
+
+dbConnection().then(
+  db => {
+    return db
+      .dropDatabase()
+      .then(() => {
+        return dbConnection;
+      })
+      //adds Dylan to database
+      .then(db => {
+        return drinks.addUser("Dylan", "diGeronimo");
+      })
+
+      //Adds Moscow Mule under Dylan's profile
+      .then(Dylan => {
+        const id = Dylan._id;
+
+        return drinks
+          .create("Moscow Mule", "Strong", "Sweet",
+          ["Vodka"], ["Ginger beer", "lime juice", "ice"],
+          "Spoon", "Copper Mug", "Combine 4 oz Ginger beer, 1 1/2 oz Vodka, and 1/6 oz Lime juice in mug with ice. Stir to combine",
+          "Medium", 4)
+
+          .then(() => {
+            return drinks.create();
+          })
+
+          .then(() => {
+            return drinks.create();
+          });
+      })
+
+      .then(() => {
+        console.log("Done seeding database");
+        db.close();
+      });
+  },
+  error => {
+    console.error(error);
+  }
+);

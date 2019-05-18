9f84b624e30a935958675b35f99fdcecd88857d1
tmitche2 <36667814+tmitche2@users.noreply.github.com>
2019-05-18 18:26:08 -0400

    Create login.js

---
+++ routes/login.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+router.get("/", (req, res) => {
+    const info = req.body;
+    if(!info.username){
+        //errors if there is no username given
+        res.render("layouts/loginError",{error:"empty username"});
+        //check here if broken
+        return 1;
+    }
+    if(!info.password){
+        //errors if there is no password given
+        res.render("layouts/loginError",{error:"empty password"});
+        return 1;
+    }
+    let user = data.filter(function(user){
+        return user.username === info.username;
+    });
+    let passwordTruth = bcrypt.compare(info.password, user[0].hashedPassword);
+    if(user[0]!= undefined && passwordTruth){
+        //checks the password against the hashed password, and searches for user
+        //password correct
+        let newUser = {...user[0]}
+        delete newUser.hashedPassword;
+        delete newUser.Password;
+        req.session.user = newUser;
+        res.render("layouts/submitRecipe");
+    }else{
+        //username or password was incorrect
+        res.render("layouts/loginError",{error:"username or password error"});
+        return;
+    }
+});
+router.get("*", (req,res)=>{
+    res.render("layouts/submitRecipe");
+});
+module.exports=router;

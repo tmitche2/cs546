5d80ac467fa72de3513d4f17e67adc1aee21e242
tmitche2 <36667814+tmitche2@users.noreply.github.com>
2019-05-18 18:25:53 -0400

    creating new files for functions called(login,etc)

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,7 @@ const drinks = require("../data/drinks");
 const users = require("../data/users");
 const bcrypt = require("bcrypt");
 
+const logins = require("./login");
 
 // Constructor
 const constructorMethod = app => {
@@ -98,45 +99,16 @@ const constructorMethod = app => {
         res.render("layouts/submitRecipe");
     });
 
+    app.use("/login", logins);
+
     app.use("/", (req, res) => {
         res.render("layouts/search");
         return;
     });
-
-    app.post("/login", async (req, res) => {
-        const info = req.body;
-        if(!info.username){
-            //errors if there is no username given
-            res.render("layouts/loginError",{error:"empty username"});
-            //check here if broken
-            return 1;
-        }
-        if(!info.password){
-            //errors if there is no password given
-            res.render("layouts/loginError",{error:"empty password"});
-            return 1;
-        }
-        let user = data.filter(function(user){
-            return user.username === info.username;
-        });
-        let passwordTruth = await bcrypt.compare(info.password, user[0].hashedPassword);
-        if(user[0]!= undefined && passwordTruth){
-            //checks the password against the hashed password, and searches for user
-            //password correct
-            let newUser = {...user[0]}
-            delete newUser.hashedPassword;
-            delete newUser.Password;
-            req.session.user = newUser;
-            res.redirect("layouts/submitRecipe");
-        }else{
-            //username or password was incorrect
-            res.render("layouts/loginError",{error:"username or password error"});
-            return;
-        }
-    });
     app.use("*", (req, res) => {
         res.status(404);
     });
+    
 };
 
 module.exports = constructorMethod;

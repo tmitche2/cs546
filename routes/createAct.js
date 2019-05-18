e8e34d23597db733b3de2500c83d49921322e578
tmitche2 <36667814+tmitche2@users.noreply.github.com>
2019-05-18 19:10:26 -0400

    Create createAct.js

---
+++ routes/createAct.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const users = require("../data/users");
+
+router.post("/", (req, res) => {
+    const info = req.body;
+    console.log("FUCKing hell");
+    if(!info.name){
+        //errors if there is no username given
+        res.render("layouts/loginError",{error:"No username given"});
+        //check here if broken
+        return 1;
+    }
+    if(!info.password){
+        //errors if there is no password given
+        res.render("layouts/loginError",{error:"No password given"});
+        return 1;
+    }
+    if(!info.age){
+        //errors if there is no password given
+        res.render("layouts/loginError",{error:"You must give an age"});
+        return 1;
+    }
+});
+router.get("*", (req,res)=>{
+    res.render("layouts/submitRecipe");
+});
+module.exports=router;

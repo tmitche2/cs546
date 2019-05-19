9387684174a0ae1dd128599e55df45c597d25b79
nmiah <nmiah>
2019-05-18 21:18:14 -0400

    error checking on findrecipe

--- routes/findrecipe.js
+++ routes/findrecipe.js
@@ -9,24 +9,39 @@ router.post("/", async (req, res) => {
     const ingredients = req.body.ingredients;
     const difficulty = req.body.difficulty;
     const strength = req.body.strength;
+    console.log("I got here 1");
     if(!alcoholTypes){
         res.render("layouts/loginError",{error:"No alcohol given. We only give alcoholic drinks."});
         //check here if broken
         return 1;
     } else {
-        let drink = await drinks.filterDrinks(alcoholTypes, ingredients, difficulty, strength); 
-        res.render("layouts/drink", {
-            drinkName: drink.drinkName,
-            strength: drink.strength,
-            flavor: drink.flavor,
-            alcoholTypes: drink.alcoholList,
-            ingredients: drink.ingredientList,
-            tools: drink.toolList,
-            glassType: drink.glassType,
-            prepInfo: drink.prepInfo,
-            difficulty: drink.difficulty,
-            rating: drink.rating
-        });
+        console.log("I got here 2");
+        try {
+            console.log("I got here 3");
+            let drinkList = await drinks.filterDrinks(alcoholTypes, ingredients, difficulty, strength);
+            const topResult = drinkList.shift();
+            console.log("I got here 4");
+            res.redirect("layouts/drink", {
+                drinkName: topResult.drinkName,
+                strength: topResult.strength,
+                flavor: topResult.flavor,
+                alcoholTypes: topResult.alcoholList,
+                ingredients: topResult.ingredientList,
+                tools: topResult.toolList,
+                glassType: topResult.glassType,
+                prepInfo: topResult.prepInfo,
+                difficulty: topResult.difficulty,
+                rating: topResult.rating
+            });
+            console.log("I got here 5");
+        } catch (error) {
+            res.render("layouts/drinkNotFound", {error:"Couldn't find drinks"});
+            return 1; 
+        }
+        
+        console.log("I got here 6");
+
+        
     }
 });
 router.get("*", (req,res)=>{
